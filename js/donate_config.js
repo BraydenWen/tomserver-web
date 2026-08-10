1ceb874b8addccfaec910bc8e0d30bc1db13d490
Brayden Wen <wxt060723@163.com>
2026-08-10 13:49:01 +0800

    Add files via upload

--- js/donate_config.js
+++ js/donate_config.js
@@ -14,7 +14,7 @@ const AppConfig = {
     
     // 收款码图片
     paymentQrCodes: {
-        wechat: "https://cdn.jsdelivr.net/gh/BraydenWen/tomserver-web@refs/heads/main/picture/paywechat.png",
-        alipay: "https://cdn.jsdelivr.net/gh/BraydenWen/tomserver-web@refs/heads/main/picture/payalipay.png"
+        wechat: "https://shp.qpic.cn/collector/2475736084/54d0518c-d5e5-4648-9995-4f46a5e21246/0",
+        alipay: "https://shp.qpic.cn/collector/2475736084/6bd916da-8f8a-44fa-ba44-0dd7e1ef5f5d/0"
     }
 };
